2f49c1a84699d52296cf8fdf3cef9ff00ffe2ba8
Marco "Capypara" Köpcke <hello@capypara.de>
2024-09-02 15:54:32 +0200

    Rename executable to app ID for WM class fixes under XOrg

--- run-pmdo.sh
+++ run-pmdo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # If arguments are given, launch PMDO raw. This allows for setting a custom asset path.
 if [ $# -eq 0 ]; then
-    PMDC -asset /app/share/PMDO/Assets/ -appdata
+    org.pmdcollab.pmdo -asset /app/share/PMDO/Assets/ -appdata
 else
-    PMDC "$@" -appdata
+    org.pmdcollab.pmdo "$@" -appdata
 fi
